4f774b266cabbee96f8792462552369cc9ac21be
VitorVHP <vitor.ramos01@fatec.sp.gov.br>
2024-09-05 10:43:14 -0300

    criacao do projeto, tailwind e tabelas

---
+++ resources/views/tela-teste.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+  <title>Tela teste</title>
+</head>
+<body>
+  <h1 class="bg-black p-4 rounded-lg">
+  </h1>
+</body>
+</html>
